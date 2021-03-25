8734e6bc1986c50d50e0feecc87a23eef2c165c7
Andrew <drewg3r@gmail.com>
2021-03-25 12:18:41 +0200

    Add README.md; small fixes

---
+++ README.md
@@ -0,0 +1,56 @@
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/drewg3r/covid">
+    <img src="images/favicon.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">Covid-19 in Ukraine</h3>
+
+  <p align="center">
+    COVID-19 Coronavirus Pandemic Stats for Ukraine
+    <br />
+    <a href="https://drewg3r.github.io/covid/"><strong>Visit website »</strong></a>
+    <br />
+    <a href="https://github.com/drewg3r/covid/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/drewg3r/covid/issues">Request Feature</a>
+  </p>
+</p>
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Website Screen Shot][product-screenshot]](https://github.com/drewg3r/covid)
+
+There are a lot of information sources for COVID-19 stats in Ukraine. But all of them have overloaded interface with lots of useless information(or the interface consists of lots of graphic so its hard to view on mobile or with pure internet connection). So I decided to create a very simple webpage showing only information that matters(for me and, hopefully, for others). 
+
+This is my first experience working with JS. I just wanted to learn basics of it so I haven't used even jQuery.
+
+
+
+### Built With
+
+* [Papa Parse](https://www.papaparse.com/)
+* [Bootstrap](https://getbootstrap.com/)
+* [dmytro-derkach/covid-19-ukraine](https://github.com/dmytro-derkach/covid-19-ukraine) - using data from this repo
+
+<!-- ROADMAP -->
+## Roadmap
+
+See the [open issues](https://github.com/drewg3r/covid/issues) for a list of proposed features (and known issues).
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+<!-- CONTACT -->
+## Contact
+
+Telegram: [@drewg3r](https://t.me/drewg3r)
+
+Project Link: [https://github.com/drewg3r/covid](https://github.com/drewg3r/covid)
+
+[product-screenshot]: images/screenshot.png
--- js/covid.js
+++ js/covid.js
@@ -3,20 +3,24 @@ window.onload = fillFields();
 function fillFields(){
   var sel = document.getElementById("districtSelection").selectedIndex;
   if (sel != 0) {
-    document.getElementById("head").innerHTML = "("+document.getElementById('districtSelection').options[sel].text+")"
+    document.getElementById("head").innerHTML = "("+document.getElementById('districtSelection').options[sel].text+")";
   } else {
-    document.getElementById("head").innerHTML = ""
+    document.getElementById("head").innerHTML = "";
   }
+
   Papa.parse("https://raw.githubusercontent.com/dmytro-derkach/covid-19-ukraine/master/cases.csv", {
     download: true,
     complete: function(results) {
       fillSummary(results.data);
+      var isDataDeprecated = 0;
       if (new Date(results.data[3][4]).getDate() == new Date().getDate()) {
         document.getElementById("updatedStatus").innerHTML = "(&#10004; оновлено сьогодні)";
+        isDataDeprecated = 0;
       } else {
         document.getElementById("updatedStatus").innerHTML = "(&#10006; оновлено вчора)";
+        isDataDeprecated = 1;
       }
-      fillHistory();
+      fillHistory(isDataDeprecated);
     }
   });
 }
@@ -56,27 +60,25 @@ function fillSummary(results){
     recoveredDelta += Number(results[sel][13]);
     activeCasesDelta += Number(results[sel][14]);
   }
-  document.getElementById("totalCases").innerHTML = new Intl.NumberFormat('ua-UA').format(totalCases);
-  document.getElementById("totalActiveCases").innerHTML = new Intl.NumberFormat('ua-UA').format(totalActiveCases);
-  document.getElementById("activeCasesDelta").innerHTML = (activeCasesDelta<0?"":"+") + new Intl.NumberFormat('ua-UA').format(activeCasesDelta);
-  document.getElementById("newCases").innerHTML = new Intl.NumberFormat('ua-UA').format(newCases);
-  document.getElementById("recoveredDelta").innerHTML = new Intl.NumberFormat('ua-UA').format(recoveredDelta);
-  document.getElementById("deathsDelta").innerHTML = new Intl.NumberFormat('ua-UA').format(deathsDelta);
-  document.getElementById("totalRecovered").innerHTML = new Intl.NumberFormat('ua-UA').format(totalRecovered);
-  document.getElementById("totalDeaths").innerHTML = new Intl.NumberFormat('ua-UA').format(totalDeaths);
+
+  var dateFormat = new Intl.NumberFormat('ua-UA');
+
+  document.getElementById("totalCases").innerHTML = dateFormat.format(totalCases);
+  document.getElementById("totalActiveCases").innerHTML = dateFormat.format(totalActiveCases);
+  document.getElementById("activeCasesDelta").innerHTML = (activeCasesDelta<0?"":"+") + dateFormat.format(activeCasesDelta);
+  document.getElementById("newCases").innerHTML = dateFormat.format(newCases);
+  document.getElementById("recoveredDelta").innerHTML = dateFormat.format(recoveredDelta);
+  document.getElementById("deathsDelta").innerHTML = dateFormat.format(deathsDelta);
+  document.getElementById("totalRecovered").innerHTML = dateFormat.format(totalRecovered);
+  document.getElementById("totalDeaths").innerHTML = dateFormat.format(totalDeaths);
 
 }
 
-
-function pad(number) {
-  if (number < 10) {
-    return '0' + number;
-  }
-  return number;
-}
-
-function fillHistory(){
+function fillHistory(isDataDeprecated){
   var date = new Date();
+
+  date.setDate(date.getDate() - Number(isDataDeprecated));
+
   var table = document.getElementById("tableHistory"); 
 
   var totalActiveCases = 0;
@@ -90,7 +92,11 @@ function fillHistory(){
 
   for (let i = 0; i < 7; i++) {
     date.setDate(date.getDate() - 1);
-    var cdate = pad(Number(date.getMonth())+1) + "-" + pad(date.getDate()) + "-"+pad(date.getFullYear()) + ".csv";
+
+    var cdate = pad(Number(date.getMonth())+1) + "-"
+      +pad(date.getDate()) + "-"
+      +pad(date.getFullYear()) + ".csv";
+    
     Papa.parse("https://raw.githubusercontent.com/dmytro-derkach/covid-19-ukraine/master/daily_reports/"+cdate, {
       download: true,
       worker: false,
@@ -112,14 +118,24 @@ function fillHistory(){
           activeCasesDelta += Number(results[sel][14]);
         }
 
-        totalActiveCases = new Intl.NumberFormat('ua-UA').format(totalActiveCases);
-        newCases = new Intl.NumberFormat('ua-UA').format(newCases);
-        activeCasesDelta = (activeCasesDelta<0?"":"+") + new Intl.NumberFormat('ua-UA').format(activeCasesDelta);
+        var dateFormat = new Intl.NumberFormat('ua-UA');
+
+        totalActiveCases = dateFormat.format(totalActiveCases);
+        newCases = dateFormat.format(newCases);
+        activeCasesDelta = (activeCasesDelta<0?"":"+") + dateFormat.format(activeCasesDelta);
 
         let idate = new Date();
         idate.setDate(idate.getDate() - (i+1));
-        let icdate = pad(Number(idate.getMonth())+1) + "-" + pad(idate.getDate()) + "-"+pad(idate.getFullYear());
-        tableBody.set(i, "<tr><th scope='row'>"+icdate+"</th><td>"+newCases+"</td><td>"+activeCasesDelta+"</td><td>"+totalActiveCases+"</td></tr>");
+
+        let icdate = pad(idate.getDate()) + "-"
+          +pad(Number(idate.getMonth())+1) + "-"
+          +pad(idate.getFullYear());
+
+        tableBody.set(i, "<tr><td class='text-nowrap'><b>"+icdate
+          +"</b></td><td class='text-nowrap'>"+newCases
+          +"</td><td class='text-nowrap'>"+activeCasesDelta
+          +"</td><td class='text-nowrap'>"+totalActiveCases+"</td></tr>");
+
         if (tableBody.size == 7) {
           for (let i = 0; i < 7; i++) {
             tableBodyContent += tableBody.get(i);
@@ -129,4 +145,11 @@ function fillHistory(){
       }
     });
   }
+}
+
+function pad(number) {
+  if (number < 10) {
+    return '0' + number;
+  }
+  return number;
 }
